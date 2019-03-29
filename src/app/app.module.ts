4d16f8defeda75ddb58c358998a5ed56fcb0b479
Harry Thuku <harsygeorgee@gmail.com>
2019-03-29 06:11:07 +0300

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {GitsearchService} from "./gitsearch.service";
+import { RouterModule,Routes} from "@angular/router";
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RepositoryDisplayComponent } from './repository-display/repository-display.component';
@@ -9,6 +12,13 @@ import { UserComponent } from './user/user.component';
 import { UserDisplayComponent } from './user-display/user-display.component';
 import { UserSearchComponent } from './user-search/user-search.component';
 import { DateformatPipe } from './dateformat.pipe';
+
+const appRoutes: Routes = [
+  { path: 'Users', component: UserSearchComponent},
+  { path: 'Repos', component:  RepositorySearchbarComponent},
+  { path: 'user/:uname', component:  UserComponent},
+  { path: '', redirectTo: 'user/HarryThuku',pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -22,9 +32,15 @@ import { DateformatPipe } from './dateformat.pipe';
     DateformatPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
   ],
-  providers: [],
+  providers: [GitsearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
